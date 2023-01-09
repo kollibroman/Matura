975097800a8ce9351d16a8a8bf87771030e4cd88
Filip <filipkwiek888@gmail.com>
2023-01-09 16:02:42 +0100

    Cleanup

---
+++ Zadania_Matura/Zad4_2022/Zad4_1.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include<map>
+
+using namespace std;
+
+int main()
+{
+    
+    string linie;   //4.1
+    vector<string> liczby;
+    vector<string> wazne_liczby;
+
+    vector<int> liczby_int;
+    vector<int> czynniki;
+
+    int liczba_liczb = 0;
+
+    ifstream plik("przyklad.txt");
+
+    //4.1
+    if(!plik.good())
+    {
+        cout << "Dupa" << endl;
+    }
+
+    while (!plik.eof())
+    {
+        getline(plik, linie);
+        liczby.push_back(linie);
+    }
+
+    for(int i = 0; i < liczby.size() - 1; i++)  //szukanie liczby ktorej pierwsza cyfra jest taka sama jak ostatnia
+    {
+        string temp = liczby[i];
+
+        if(temp[0] ==  temp[temp.length() - 2])
+        {
+            wazne_liczby.push_back(temp);
+            liczba_liczb++;
+        }
+    }
+
+    cout << "ilosc liczb: " << liczba_liczb << endl;
+    cout << "Pierwsza liczba: " << wazne_liczby[0] << endl;
+
+    return 0;
+
+}
--- Zadania_Matura/Zad4_2022/Zad4_2.cpp
+++ Zadania_Matura/Zad4_2022/Zad4_2.cpp
@@ -2,12 +2,11 @@
 #include<fstream>
 #include<vector>
 #include<map>
-
 using namespace std;
 
-vector<int> rozklad_na_czynniki(int n)
-{
-    vector<int> czynniki;
+vector<int> rozklad_na_czynniki(int n)      //TODO: policzyc dzielniki i znalezc ich najwieksza liczbe i dodac do zmiennej
+{                                           // Wrzucic w osobny plik btw
+    vector<int> czynniki;                   // aby sprawdzic dzielniki rozne wystarczy poprzedni z nastepnym porownywac i do zmiennej dodawac
     int prime = 2;
     while (n > 1)
     {
@@ -23,51 +22,16 @@ vector<int> rozklad_na_czynniki(int n)
 
 int main()
 {
-    
-    string linie;   //4.1
-    vector<string> liczby;
-    vector<string> wazne_liczby;
-
-    vector<int> liczby_int;
-    vector<int> czynniki;
-
-    int liczba_liczb = 0;
-
-    ifstream plik("przyklad.txt");
-
     string linie2;  //4.2
     int liczby_rozne = 0;
+    vector<string> liczby;
+    vector<int> liczby_int;
+    vector<int> czynniki;
     vector<string> czynnikowe_liczby; 
     map<string, string> pary_liczb;
 
     ofstream of_plik2("liczby_z_cznnikami.txt");
     ifstream if_plik2("liczby_z_cznnikami.txt");
-
-    //4.1
-    if(!plik.good())
-    {
-        cout << "Dupa" << endl;
-    }
-
-    while (!plik.eof())
-    {
-        getline(plik, linie);
-        liczby.push_back(linie);
-    }
-
-    for(int i = 0; i < liczby.size() - 1; i++)  //szukanie liczby ktorej pierwsza cyfra jest taka sama jak ostatnia
-    {
-        string temp = liczby[i];
-
-        if(temp[0] ==  temp[temp.length() - 2])
-        {
-            wazne_liczby.push_back(temp);
-            liczba_liczb++;
-        }
-    }
-
-    cout << "ilosc liczb: " << liczba_liczb << endl;
-    cout << "Pierwsza liczba: " << wazne_liczby[0] << endl;
 
     //4.2
     for(int j = 0; j < liczby.size() - 1; j++) //konwersja
@@ -134,8 +98,4 @@ int main()
         string temp = czynnikowe_liczby[i];
         of_plik2 << temp << pary_liczb[temp] << endl;
     }
-    
-
-    return 0;
-
 }
