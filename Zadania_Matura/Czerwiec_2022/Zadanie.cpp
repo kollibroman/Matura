0f4b372a4c407dbd63d8c3586e2ede9d4c51b21a
kollibroman <filipkwiek888@gmail.com>
2023-03-23 20:43:30 +0000

    Did task 4_06_2022

---
+++ Zadania_Matura/Czerwiec_2022/Zadanie.cpp
@@ -0,0 +1,150 @@
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+bool czy_podzielne_17(int n)
+{
+    if(n % 17 == 0)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+string Odwrocona_liczba(string str)
+{
+    string reverse_str = "";
+
+    for(int i = str.length() - 1; i >= 0; i--)
+    {
+        reverse_str += str[i];
+    }
+
+    return reverse_str;
+}
+
+bool czy_pierwsza(int n)
+{
+    for (int i = 2; i < n; i++)
+    {
+        if(n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    string linia;
+    vector<string> liczby;
+
+    ifstream plik("liczby.txt");
+    ofstream plik2("odp.txt");
+
+    vector<int> podzielne_odbicia;
+
+    int Max_roznica = 0;
+    int Max_liczba;
+
+    vector<int> liczba;
+    vector<int> odbicie;
+
+    int rozne = 0;
+    int podwojne = 0;
+    int potrojne = 0;
+
+    while(!plik.eof())
+    {
+        getline(plik, linia);
+
+        if(linia != "")
+        {
+            liczby.push_back(linia);
+        }
+    }
+
+    plik.close();
+
+    for(int i = 0; i < liczby.size(); i++)
+    {
+       if(czy_podzielne_17(stoi(Odwrocona_liczba(liczby[i]))))
+       {
+            podzielne_odbicia.push_back(stoi(Odwrocona_liczba(liczby[i])));
+       }
+    }
+
+    for (int i = 0; i < liczby.size(); i++)
+    {
+        int a = stoi(liczby[i]);
+        int b = stoi(Odwrocona_liczba(liczby[i]));
+
+        int roznica = abs(a - b);
+
+        if(roznica > Max_roznica)
+        {
+            Max_roznica = roznica;
+            Max_liczba = a;
+        }
+    }
+    
+    for(int i = 0; i < liczby.size(); i++)
+    {
+        int temp = stoi(liczby[i]);
+        int temp1 = stoi(Odwrocona_liczba(liczby[i]));
+
+        if(czy_pierwsza(temp) && czy_pierwsza(temp1))
+        {
+            liczba.push_back(temp);
+            odbicie.push_back(temp1);
+        }
+    }
+
+    sort(liczby.begin(), liczby.end());
+
+    for(int i = 0; i < liczby.size(); i++)
+    {
+        plik2 << liczby[i] << endl;
+
+        if(liczby[i] != liczby[i + 1])
+        {
+            rozne++;
+        }
+
+        else if(i != 0 && liczby[i] == liczby[i - 1])
+        {
+            podwojne++;
+        }
+
+        else if(liczby[i] == liczby[i + 1] && liczby[i] == liczby[i + 2])
+        {
+            potrojne++;
+        }
+       
+    }
+
+    plik2.close();
+
+    cout << "4.1" << endl;
+
+    for (int i = 0; i < podzielne_odbicia.size(); i++)
+    {
+        cout << podzielne_odbicia[i] << endl;
+    }
+
+    cout << "4.2" << " " << Max_liczba << " " << Max_roznica << endl;
+
+    cout << "4.3" << endl;
+
+    for(int i = 0; i < liczba.size(); i++)
+    {
+        cout << liczba[i] << " " << odbicie[i] << endl;
+    }
+
+    cout << "4.4" << " " << rozne << " " << podwojne << " " << potrojne << endl;
+    return 0;
+}
--- Zadania_Matura/Zad4_2019/Zad4.cpp
+++ Zadania_Matura/Zad4_2019/Zad4.cpp
@@ -29,15 +29,58 @@ int suma_silnii(string str)
     return suma;
 }
 
+int NWD(int x, int y)
+{
+    while(x != y)
+    {
+        if(x > y)
+        {
+            x -= y;
+        }
+        else 
+        {
+            y -= x;
+        }
+    }
+
+    return x;
+}
+
+int NWD_ciag(vector<int> vec)
+{
+    for (int i = 1; i < vec.size(); i++)
+    {
+        while(vec[i] != vec[i - 1])
+        {
+            if(vec[i] > vec[i - 1])
+            {
+                vec[i] -= vec[i - 1];
+            }
+
+            else
+            {
+                vec[i - 1] -= vec[i];
+            }
+        }
+    }   
+}
+
 int main()
 {
     string linia;
     vector<int> liczby;
     vector<int> _odp;
-    ifstream plik("liczby.txt");
+    ifstream plik("przyklad.txt");
 
     int licznik_poteg = 0;
-    int temp_sil = 0;
+    int ciag = 0;
+
+    int Max_ciag;
+    int Max_dziel;
+
+    int dzielnik;
+
+    vector<int> ciag_l;
 
     while (!plik.eof())
     {
@@ -49,6 +92,8 @@ int main()
         }
     }
 
+    plik.close();
+    
     //4.1
     for (int i = 0; i <= liczby.size(); i++)
     {
@@ -71,6 +116,18 @@ int main()
             _odp.push_back(temp);
         }
     }
+
+    for(int i = 1; i < liczby.size(); i++)
+    {   
+        
+
+        if(ciag > Max_ciag)
+        {
+            Max_ciag = ciag;
+        }
+
+        ciag = 0;
+    }
     
         
     cout << "4.1" << " " << licznik_poteg << endl;
@@ -80,6 +137,8 @@ int main()
     {
         cout << _odp[i] << endl;
     }
+
+    cout << "4.3" << " " << Max_ciag << " " << Max_dziel << endl;
     
     return 0;
 }
